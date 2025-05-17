1dfdd0af85bcadb1cb5bd461ba8104272d7fdfde
valmo-dev <valmo.dev@gmail.com>
2025-05-17 14:13:03 +0200

    feat: add SectionTitle component for displaying section headers

---
+++ src/components/SectionTitle.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex flex-col w-full">
+    <h1 class="text-8xl font-bold uppercase text-primary z-10 ml-12">Developpeur</h1>
+    <h1
+      class="text-8xl font-bold uppercase text-transparent custom-text-outline-white self-end -mt-10 mr-12"
+    >
+      Fullstack
+    </h1>
+  </div>
+</template>
