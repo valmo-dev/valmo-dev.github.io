f038ed583cec3ca7b447025529450f9994e8e158
valmo-dev <valmo.dev@gmail.com>
2025-05-17 14:51:18 +0200

    style(HomeView): adjust max-width for better layout consistency

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,7 @@ import SectionTitle from '../components/SectionTitle.vue'
 </script>
 
 <template>
-  <main class="py-8 w-full max-w-5xl mx-auto flex flex-col gap-20">
+  <main class="w-full max-w-7xl mx-auto flex flex-col gap-20">
     <Navbar />
     <SectionTitle />
   </main>
