a61de10ff3e8deef9e843cb458d93621036ad8c3
valmo-dev <valmo.dev@gmail.com>
2025-05-17 14:51:55 +0200

    style(SectionTitle): adjust width and center align section title

--- src/components/SectionTitle.vue
+++ src/components/SectionTitle.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col w-full">
+  <div class="flex flex-col w-full max-w-5xl mx-auto">
     <h1 class="text-8xl font-bold uppercase text-primary z-10 ml-12">Developpeur</h1>
     <h1
       class="text-8xl font-bold uppercase text-transparent custom-text-outline-white self-end -mt-10 mr-12"
