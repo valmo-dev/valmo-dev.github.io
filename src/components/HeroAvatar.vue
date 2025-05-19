570137064daf1c2d1e44833034241d449bb8f04f
valmo-dev <valmo.dev@gmail.com>
2025-05-19 16:13:17 +0200

    feat: add HeroAvatar component for displaying user avatar

---
+++ src/components/HeroAvatar.vue
@@ -0,0 +1,11 @@
+<template>
+  <div
+    class="relative rounded-full w-[240px] h-[240px] xs:w-[280px] xs:h-[280px] sm:w-[320px] sm:h-[320px] md:w-[400px] md:h-[400px] lg:w-[480px] lg:h-[480px] bg-secondary border-3 border-black shadow-[8px_8px_0_#000000] flex items-center justify-center overflow-hidden"
+  >
+    <img
+      src="../assets/avatar-valmo.png"
+      alt="Avatar de Valentin Moreau"
+      class="w-full h-full object-cover"
+    />
+  </div>
+</template>
