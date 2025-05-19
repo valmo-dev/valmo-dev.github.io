48248a4203352746448dac70bceeba4271f449d5
valmo-dev <valmo.dev@gmail.com>
2025-05-19 16:13:01 +0200

    feat(NavBar): add mobile menu toggle functionality

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,11 +1,18 @@
 <script setup>
 import { RouterLink } from 'vue-router'
+import { ref } from 'vue'
+
+const isMobileMenuOpen = ref(false)
+
+const toggleMobileMenu = () => {
+  isMobileMenuOpen.value = !isMobileMenuOpen.value
+}
 </script>
 
 <template>
   <nav class="left-0 top-0 z-50 w-full px-4">
     <div
-      class="mx-auto mt-4 h-[80px] w-full max-w-screen border-3 border-black shadow-[8px_8px_0_#000000] text-white flex justify-between items-center px-6 transition-transform duration-300 ease-in-out transform translate-y-0"
+      class="mx-auto mt-4 h-[80px] w-full max-w-screen bg-background-primary border-3 border-black shadow-[8px_8px_0_#000000] text-white flex justify-between items-center px-6 transition-transform duration-300 ease-in-out transform translate-y-0"
     >
       <RouterLink
         to="/"
@@ -30,7 +37,7 @@ import { RouterLink } from 'vue-router'
         >
       </div>
       <div class="flex items-center gap-4">
-        <div class="relative">
+        <div class="hidden md:block relative">
           <a
             href=""
             class="border-3 border-black bg-primary shadow-[8px_8px_0_#000000] inline-flex items-center justify-center whitespace-nowrap h-10 px-4 py-2 hover:shadow-none hover:translate-y-[8px] hover:translate-x-[8px] transition-all"
@@ -38,7 +45,54 @@ import { RouterLink } from 'vue-router'
             Contact</a
           >
         </div>
+        <div class="md:hidden">
+          <button @click="toggleMobileMenu" class="text-white focus:outline-none">
+            <svg
+              class="w-8 h-8"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M4 6h16M4 12h16m-7 6h7"
+              ></path>
+            </svg>
+          </button>
+        </div>
       </div>
+    </div>
+    <div
+      v-if="isMobileMenuOpen"
+      class="md:hidden mt-2 bg-secondary border-3 border-black shadow-[8px_8px_0_#000000]"
+    >
+      <RouterLink
+        to="/"
+        class="block px-4 py-3 text-white hover:bg-primary"
+        @click="toggleMobileMenu"
+        >Accueil</RouterLink
+      >
+      <RouterLink
+        to="/"
+        class="block px-4 py-3 text-white hover:bg-primary"
+        @click="toggleMobileMenu"
+        >Parcours</RouterLink
+      >
+      <RouterLink
+        to="/"
+        class="block px-4 py-3 text-white hover:bg-primary"
+        @click="toggleMobileMenu"
+        >Réalisation</RouterLink
+      >
+      <a
+        href=""
+        class="block px-4 py-3 text-white bg-primary hover:bg-opacity-80 font-bold text-center"
+        @click="toggleMobileMenu"
+        >Contact</a
+      >
     </div>
   </nav>
 </template>
