c342240365d06be032826721526cca32ab10c10a
valmo-dev <valmo.dev@gmail.com>
2025-05-19 17:56:04 +0200

    style(HeroAvatar): adjust image path and add margin for better layout

--- src/components/HeroAvatar.vue
+++ src/components/HeroAvatar.vue
@@ -1,9 +1,9 @@
 <template>
   <div
-    class="relative rounded-full w-[240px] h-[240px] xs:w-[280px] xs:h-[280px] sm:w-[320px] sm:h-[320px] md:w-[400px] md:h-[400px] lg:w-[480px] lg:h-[480px] bg-secondary border-3 border-black shadow-[8px_8px_0_#000000] flex items-center justify-center overflow-hidden"
+    class="relative rounded-full w-[240px] h-[240px] xs:w-[280px] xs:h-[280px] sm:w-[320px] sm:h-[320px] md:w-[400px] md:h-[400px] lg:w-[450px] lg:h-[450px] bg-secondary border-3 border-black shadow-[8px_8px_0_#000000] flex items-center justify-center overflow-hidden mt-12 md:mt-0"
   >
     <img
-      src="../assets/avatar-valmo.png"
+      src="../assets/img/avatar-valmo.png"
       alt="Avatar de Valentin Moreau"
       class="w-full h-full object-cover"
     />
