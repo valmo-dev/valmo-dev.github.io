8633059f3b12f92c175a05a3724d8aeaa392420c
valmo-dev <valmo.dev@gmail.com>
2025-05-19 17:57:25 +0200

    refactor(HomeView): update layout and add SkillsBar component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,27 +5,33 @@ import SectionTitle from '../components/SectionTitle.vue'
 import HeroText from '../components/HeroText.vue'
 import CircleDecoration from '../components/CircleDecoration.vue'
 import HeroAvatar from '@/components/HeroAvatar.vue'
+import SkillsBar from '@/components/SkillsBar.vue'
 </script>
 
 <template>
-  <main class="w-full max-w-7xl mx-auto flex flex-col gap-12 sm:gap-16 md:gap-20 px-4">
+  <main
+    class="relative flex min-h-[100vh] w-full flex-col items-center bg-background-primary bg-[linear-gradient(to_right,#80808033_1px,transparent_1px),linear-gradient(to_bottom,#80808033_1px,transparent_1px)] bg-[size:70px_70px] pt-24 lg:pt-0"
+  >
     <div
       class="absolute -top-80 scale-75 sm:scale-100 sm:-top-96 md:-top-145 -left-40 sm:-left-32 md:-left-50 z-0"
     >
       <CircleDecoration />
     </div>
-    <div class="relative z-10 flex flex-col gap-8 sm:gap-12 md:gap-16">
-      <Navbar />
-      <SectionTitle />
-      <!-- <HelloButton /> -->
-      <div class="grid grid-cols-1 md:grid-cols-3 items-center gap-6 md:gap-8 lg:gap-12">
-        <div class="col-span-2">
-          <HeroText class="order-2 md:order-1" />
-        </div>
-        <div class="col-span-1">
-          <HeroAvatar class="order-1 md:order-2" />
-        </div>
+    <!-- <div class="relative z-10 flex flex-col gap-8 sm:gap-12 md:gap-16"> -->
+    <!-- <HelloButton /> -->
+    <Navbar class="top-0 w-full max-w-7xl" />
+    <SectionTitle />
+    <div
+      class="mx-auto w-full max-w-7xl px-5 py-10 md:py-20 text-left flex flex-col lg:flex-row items-center z-10"
+    >
+      <div class="w-full lg:w-1/2 mt-8 md:mt-0 flex flex-col items-center lg:items-start lg:ml-8">
+        <HeroText />
+      </div>
+      <div class="w-full lg:w-1/2 md:mt-6 lg:mt-0 flex justify-center lg:pl-12">
+        <HeroAvatar />
       </div>
     </div>
+    <!-- </div> -->
   </main>
+  <SkillsBar />
 </template>
