1cc1de0a70212737319f8b5a9fccacee5503c846
valmo-dev <valmo.dev@gmail.com>
2025-05-19 16:11:21 +0200

    style: add text-white class to improve readability

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 
 <template>
   <div
-    class="min-h-screen opacity-100 transition-opacity duration-300 bg-background-primary font-primary font-normal"
+    class="min-h-screen opacity-100 transition-opacity duration-300 bg-background-primary font-primary font-normal text-white"
   >
     <RouterView />
   </div>
