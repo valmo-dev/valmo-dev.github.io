e58ba4b21057eedb39687acc2b92c96e027e64e7
valmo-dev <valmo.dev@gmail.com>
2025-05-19 16:12:20 +0200

    feat(components): add HelloButton component with styled button

---
+++ src/components/HelloButton.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex w-full max-w-6xl mx-auto">
+    <button
+      class="border-3 border-black bg-primary shadow-[8px_8px_0_#000000] inline-flex items-center justify-center whitespace-nowrap h-15 w-56 px-4 py-2 font-bold text-3xl -rotate-15"
+    >
+      Hello <span class="text-5xl">👋</span>
+    </button>
+  </div>
+</template>
