5e4cbbb0b2a4ca08099c740bc87f01f6ec2c1de7
valmo-dev <valmo.dev@gmail.com>
2025-05-20 17:46:34 +0200

    refactor(HeroAvatar): update avatar image path for better asset management

--- src/components/HeroAvatar.vue
+++ src/components/HeroAvatar.vue
@@ -3,7 +3,7 @@
     class="relative rounded-full w-[240px] h-[240px] xs:w-[280px] xs:h-[280px] sm:w-[320px] sm:h-[320px] md:w-[400px] md:h-[400px] lg:w-[450px] lg:h-[450px] bg-secondary border-3 border-black shadow-[8px_8px_0_#000000] flex items-center justify-center overflow-hidden mt-12 md:mt-0"
   >
     <img
-      src="../assets/img/avatar-valmo.png"
+      src="/img/avatar-valmo.png"
       alt="Avatar de Valentin Moreau"
       class="w-full h-full object-cover"
     />
