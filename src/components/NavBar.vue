6795fa941007a1b4d8b910af27f6bebd8f2c0643
valmo-dev <valmo.dev@gmail.com>
2025-05-20 18:14:05 +0200

    feat(NavBar): improve mobile menu toggle and styling

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -46,8 +46,13 @@ const toggleMobileMenu = () => {
           >
         </div>
         <div class="md:hidden">
-          <button @click="toggleMobileMenu" class="text-white focus:outline-none">
+          <button
+            @click="toggleMobileMenu"
+            class="text-white focus:outline-none relative w-8 h-8"
+            aria-label="Toggle menu"
+          >
             <svg
+              v-if="!isMobileMenuOpen"
               class="w-8 h-8"
               fill="none"
               stroke="currentColor"
@@ -58,7 +63,22 @@ const toggleMobileMenu = () => {
                 stroke-linecap="round"
                 stroke-linejoin="round"
                 stroke-width="2"
-                d="M4 6h16M4 12h16m-7 6h7"
+                d="M4 6h16M4 12h16M4 18h16"
+              ></path>
+            </svg>
+            <svg
+              v-else
+              class="w-8 h-8"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M6 18L18 6M6 6l12 12"
               ></path>
             </svg>
           </button>
@@ -67,7 +87,7 @@ const toggleMobileMenu = () => {
     </div>
     <div
       v-if="isMobileMenuOpen"
-      class="md:hidden mt-2 bg-secondary border-3 border-black shadow-[8px_8px_0_#000000]"
+      class="md:hidden mt-2 absolute top-[calc(1rem+100px)] left-4 right-4 mx-auto z-40 bg-secondary border-3 border-black shadow-[8px_8px_0_#000000]"
     >
       <RouterLink
         to="/"
@@ -89,7 +109,7 @@ const toggleMobileMenu = () => {
       >
       <a
         href=""
-        class="block px-4 py-3 text-white bg-primary hover:bg-opacity-80 font-bold text-center"
+        class="block px-4 py-3 text-white bg-primary hover:bg-opacity-70 font-bold text-center"
         @click="toggleMobileMenu"
         >Contact</a
       >
