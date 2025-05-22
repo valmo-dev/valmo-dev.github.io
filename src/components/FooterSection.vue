f1901019972a60c743659d2db1a8532846d79658
valmo-dev <valmo.dev@gmail.com>
2025-05-22 20:07:14 +0200

    feat: add FooterSection component with quick links and credits

---
+++ src/components/FooterSection.vue
@@ -0,0 +1,46 @@
+<template>
+  <footer class="w-full p-8 border-t-8 border-black">
+    <div class="mx-auto max-w-7xl">
+      <div class="grid grid-cols-1 md:grid-cols-3 gap-6 mb-12">
+        <div>
+          <h3 class="text-2xl mb-4 uppercase tracking-wider font-bold">Liens rapides</h3>
+          <ul class="space-y-3">
+            <li>
+              <RouterLink
+                class="px-3 py-1 font-bold hover:-translate-y-1 hover:rotate-2 transform transition-all duration-200"
+                to="/"
+                >Accueil</RouterLink
+              >
+            </li>
+            <li>
+              <RouterLink
+                class="px-3 py-1 font-bold hover:-translate-y-1 hover:rotate-2 transform transition-all duration-200"
+                to="/"
+                >Parcours</RouterLink
+              >
+            </li>
+            <li>
+              <RouterLink
+                class="px-3 py-1 font-bold hover:-translate-y-1 hover:rotate-2 transform transition-all duration-200"
+                to="/"
+                >Réalisation</RouterLink
+              >
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="pt-8 flex flex-col md:flex-row justify-between items-center gap-4">
+        <p class="font-semibold text-xl flex flex-row items-center gap-3">
+          <span class="text-3xl">©</span>
+          <span>2025</span>
+          <span>Valentin MOREAU</span>
+        </p>
+        <div
+          class="bg-background-primary border-3 border-black shadow-[8px_8px_0_#000000] px-4 py-2 font-mono font-semibold text-lg"
+        >
+          Code with Vue.js 3 + Tailwind
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
