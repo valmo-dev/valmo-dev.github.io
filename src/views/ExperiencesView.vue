cdc2d5a30f5862cb65c68824d5789fbc27894532
valmo-dev <valmo.dev@gmail.com>
2025-05-22 20:06:53 +0200

    chore: remove unused view components ProjectsView and ExperiencesView

--- src/views/ExperiencesView.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import SectionTitle from '../components/SectionTitle.vue'
-</script>
-
-<template>
-  <div class="">
-    <SectionTitle firstTitle="Parcours" secondTitle="Mon" />
-  </div>
-</template>
