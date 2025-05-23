ce738cadda38cbc505b7407f7a84428aba8a7405
valmo-dev <valmo.dev@gmail.com>
2025-05-23 12:25:37 +0200

    refactor(CircleDecoration): make background color dynamic via props
    This change introduces a `color` prop to dynamically set the background color of the CircleDecoration component, replacing the hardcoded `bg-secondary` class. This improves flexibility and reusability of the component.

--- src/components/CircleDecoration.vue
+++ src/components/CircleDecoration.vue
@@ -1,5 +1,15 @@
+<script setup>
+defineProps({
+  color: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
 <template>
   <div
-    class="w-[500px] h-[500px] sm:w-[650px] sm:h-[650px] md:w-[883px] md:h-[883px] rounded-full bg-secondary opacity-50 md:opacity-100"
+    class="w-[500px] h-[500px] sm:w-[650px] sm:h-[650px] md:w-[883px] md:h-[883px] rounded-full opacity-50 md:opacity-100"
+    :class="[`bg-${color}`]"
   ></div>
 </template>
