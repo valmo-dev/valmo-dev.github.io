b27cbf9ea392fd13e70f51aff5179c8ab833df93
valmo-dev <valmo.dev@gmail.com>
2025-06-10 21:07:55 +0200

    feat(StorySection): add new component for personal story section
    Introduce a new Vue component to display a personal story with styled text sections. The component includes semantic HTML structure and Tailwind CSS classes for styling.

---
+++ src/components/StorySection.vue
@@ -0,0 +1,25 @@
+<template>
+  <div
+    class="w-[90%] max-w-5xl mx-auto p-4 border-3 border-black shadow-[8px_8px_0_#000000] bg-background-primary"
+  >
+    <p class="text-xl leading-relaxed">
+      Plus jeune j'avais <span class="font-semibold text-primary">2 objectifs</span> qui me
+      semblaient à l'époque incompatible :
+      <span class="font-semibold text-primary">intégrer l'Armée de Terre</span> et
+      <span class="font-semibold text-primary"> devenir développeur. </span>
+      <br /><br />
+
+      J'ai d'abord réalisé le premier en servant comme
+      <span class="font-semibold text-primary">militaire de 2018 à 2023</span>, une expérience qui
+      m'a beaucoup appris, sur tant de plans :
+      <span class="font-semibold">
+        la rigueur, l'esprit d'équipe, le respect et la persévérance.
+      </span>
+      <br /><br />
+      Mon second objectif est toujours resté dans un coin de ma tête et, après cinq années de
+      service, j'ai pris la décision de
+      <span class="font-semibold text-primary">changer de voie</span> pour me reconvertir dans le
+      <span class="font-semibold text-primary">développement web</span>.
+    </p>
+  </div>
+</template>
